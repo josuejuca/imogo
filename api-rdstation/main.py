81edce6093c1ef5e7be82c68c773c347ef6727bf
Josué Juca <josuejuca19@gmail.com>
2024-06-07 14:44:10 -0300

    API RD Station

---
+++ api-rdstation/main.py
@@ -0,0 +1,28 @@
+import requests
+
+key = "CHAVE_API" # CHAVE_API
+url = f"https://api.rd.services/platform/conversions?api_key={key}"
+
+
+payload = {
+    "event_type": "CONVERSION",
+    "event_family": "CDP",
+    "payload": {
+        "conversion_identifier": "API DIL", # Tenta deixar assim para eu conseguir identificar o que chegou da Dil 
+        "email": "etc@josuejuca.com", # E-mail do LEAD
+        "name": "Josué Juca", # Nome do LEAD
+        "state": "DF", # Estado 
+        "city": "Brasilia", # Cidade 
+        "country": "Brasil", # Pais 
+        "personal_phone": "+55 61 9 8463-4855", # Telefone mask: +55 61 9 0000-0000
+        "tags": ["CORRETOR", "prospecção"], # Altere o campo de acordo com o tipo de LEAD usando o ( CORRETOR / LEAD )
+    }
+}
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.text)
